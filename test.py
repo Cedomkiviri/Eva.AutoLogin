0491ad01889ddca33ee5a655c7e17e7f15d9dcbf
CesarEnc <andresencmejia@hotmail.com>
2020-04-09 07:48:33 -0400

    refraction+(+doc)+full_español

--- test.py
+++ test.py
@@ -1,32 +1,31 @@
-# resources.txt : Guarda la matricula y contraseña del usuario
+# datos.txt : Guarda la matricula y contraseña del usuario
 # geckodriver.exe : Necesario para abrir Firefox a travez de selenium
 import time
 import selenium
 from selenium import webdriver
-from tkinter import * 
+from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
 import os
 
 # Esta funcion devuelve un string
-# el contenido del archivo
-# resources.txt
-def getResources():
-     f=open("resources.txt")
+# un lista [usuario,contrasena] del archivo datos.txt
+def ObtenerDatos():
+     f=open("datos.txt")
      l =f.read()
      f.close()
      return l.split(":")
 
 # Esta funcion comprueba si el archivo
-# resources.txt esta vacío 
+# datos.txt esta vacío 
 # retorna booleano
-def isResourcesEmpty():
-    return os.stat('resources.txt').st_size == 0
+def NoHayDatos():
+    return os.stat('datos.txt').st_size == 0
 
 # Este es el script que usando la libreria 
 # Selenium se conecta con Firefox a traves
 # de geckodriver.exe 
-def scriptCompleto(usuario,password):
+def scriptCompleto(usuario,contrasena):
     driver = webdriver.Firefox(executable_path="geckodriver.exe")
     driver.get('https://eva.unapec.edu.do/moodle/login/index.php')
     loginuno = driver.find_element_by_class_name('btn-login')
@@ -38,9 +37,9 @@ def scriptCompleto(usuario,password):
     userlogin.click()
     time.sleep(1)
     campoLogin = driver.find_element_by_id('i0118')
-    campoLogin.send_keys(password) 
-    passwordlogin = driver.find_element_by_id('idSIButton9')
-    passwordlogin.click()
+    campoLogin.send_keys(contrasena) 
+    contrasenalogin = driver.find_element_by_id('idSIButton9')
+    contrasenalogin.click()
     time.sleep(1)
     botonFinal = driver.find_element_by_id('idBtn_Back')
     botonFinal.click()
@@ -50,27 +49,27 @@ def scriptCompleto(usuario,password):
 # para  el registro
 def Registro():
     #Habra que cambiar esto para añadir un frame y adaptar el diseño
-    window = Toplevel(mainv)
-    window.update()
-    window.title("Registro")
-    window.geometry('700x200')
-    window.resizable(width=False, height=False)
-    window.configure(bg="white")
+    registrov = Toplevel(mainv)
+    registrov.update()
+    registrov.title("Registro")
+    registrov.geometry('700x200')
+    registrov.resizable(width=False, height=False)
+    registrov.configure(bg="white")
 
     # Esta función verifica si los campos fueron llenados,
     # si estos campos son validos crea los datos en
-    # el archivo resources.txt
+    # el archivo datos.txt
     def RegistrarUsuario(a,b):
-        if(isResourcesEmpty()):
+        if(NoHayDatos()):
             if(a== "" or b== ""):
                 messagebox.showinfo("Error", "Nombre y/o contraseña faltante")
-                window.lift()
+                registrov.lift()
             else:
-                f=open("resources.txt","w")
+                f=open("datos.txt","w")
                 f.write(a+":"+b)
                 f.close()
-                window.destroy()
-                window.update()
+                registrov.destroy()
+                registrov.update()
                 mainv.lift()
                 boton['state'] =NORMAL
         else:
@@ -78,23 +77,22 @@ def Registro():
             
     load = Image.open("0.png")
     render = ImageTk.PhotoImage(load)
-    img = Label(window, image=render)
+    img = Label(registrov, image=render)
     img.image = render
     img.place(x=0, y=0)
 
-    Label(window, text = "Matricula:",font=("Helvetica", 22)).pack()
-    correo = Entry(window)
+    Label(registrov, text = "Matricula:",font=("Helvetica", 22)).pack()
+    correo = Entry(registrov)
     correo.pack()
 
-    Label(window, text = "Contraseña:",font=("Helvetica", 22)).pack()
-    contra = Entry(window, show = "*")
+    Label(registrov, text = "Contraseña:",font=("Helvetica", 22)).pack()
+    contra = Entry(registrov, show = "*")
     contra.pack()
     
-    Button(window,text = "Login",font=("Helvetica", 22),command= lambda: RegistrarUsuario(correo.get(),contra.get())).pack()    
+    Button(registrov,text = "Login",font=("Helvetica", 22),command= lambda: RegistrarUsuario(correo.get(),contra.get())).pack()    
           
 
 #Aqui se genera la pestaña principal
-
 #Habra que cambiar esto para añadir un frame y adaptar el diseño
 mainv = Tk()
 mainv.title("Eva AutoLogin")
@@ -107,10 +105,13 @@ render = ImageTk.PhotoImage(load)
 img = Label(mainv, image=render)
 img.image = render
 img.place(x=0, y=0)
-boton = Button(mainv,text = "Abrir Eva",state=DISABLED,font=("Helvetica", 22),command= lambda : scriptCompleto(getResources()[0],getResources()[1]))
+boton = Button(mainv,text = "Abrir Eva",state=DISABLED,font=("Helvetica", 22),command= lambda : scriptCompleto(ObtenerDatos()[0],ObtenerDatos()[1]))
 boton.pack()
 
-if(isResourcesEmpty()):
+
+#Si datos.txt esta vacío, ejecuta la vista de registro
+#Sino, cambia el estado del boton
+if(NoHayDatos()):
     Registro() 
 else:
     boton['state'] =NORMAL
